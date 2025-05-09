ac8901f8d95c8b6d2a9ed202589a0d21820c0e39
Dominic Mason <masond4@vcu.edu>
2025-05-08 23:37:09 -0400

    Set up frontend with Jinja2 templating

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from database import get_post, insert_post
 from models import Posts, Post
 from sqlite3 import Connection, Row
@@ -6,12 +8,17 @@ from sqlite3 import Connection, Row
 app = FastAPI()
 connection = Connection('social.db')
 connection.row_factory = Row
+templates = Jinja2Templates(directory="./templates")
+
+@app.get('/')
+async def home(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(request = request, name = "index.html", context = {})
 
 @app.get("/posts")
 async def posts() -> Posts:
     return get_post(connection)
 
 @app.post("/posts")
-async def add_post(post : Post) -> Post:
+async def add_post(post: Post) -> Post:
     insert_post(connection, post)
     return post
