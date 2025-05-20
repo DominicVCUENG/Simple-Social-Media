60b312559c76585d5e1353288f7577564b0fa431
Dominic Mason <masond4@vcu.edu>
2025-05-19 20:31:01 -0400

    Implemented HTMX asynchonous scripting to HTML file, modified app endpoints to now return HTML responses, added UserPost to model schema

--- app.py
+++ app.py
@@ -2,23 +2,29 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from database import get_post, insert_post
-from models import Posts, Post
+from models import Posts, Post, UserPost
 from sqlite3 import Connection, Row
 
 app = FastAPI()
 connection = Connection('social.db')
 connection.row_factory = Row
+
 templates = Jinja2Templates(directory="./templates")
+user_id = 1
 
 @app.get('/')
 async def home(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse(request = request, name = "index.html", context = {})
+    posts = get_post(connection)
+    return templates.TemplateResponse(request = request, name = "index.html", context = posts.model_dump())
 
 @app.get("/posts")
-async def posts() -> Posts:
-    return get_post(connection)
+async def posts(request: Request) -> HTMLResponse:
+    posts = get_post(connection)
+    return templates.TemplateResponse(request = request, name = "posts.html", context = posts.model_dump())
 
-@app.post("/posts")
-async def add_post(post: Post) -> Post:
+@app.post("/post")
+async def add_post(post: UserPost, request: Request) -> HTMLResponse:
+    post = Post(user_id = user_id, **post.model_dump() )
     insert_post(connection, post)
-    return post
+    posts = get_post(connection)
+    return templates.TemplateResponse(request = request, name = "posts.html", context = posts.model_dump())
